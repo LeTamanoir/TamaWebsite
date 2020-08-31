13aced3a4dfd99a45eddb3fe1241429903152f1b
Tamanoir <martin.saldinger@gmail.com>
2020-09-01 01:26:41 +0200

    d

--- main.js
+++ main.js
@@ -1,60 +1,33 @@
-function defineAcceuil() {
-window.onscroll = function(e) {
-if (this.oldScroll > this.scrollY == false) {
-	backgroundTransition(true);
-}
-else if (this.oldScroll < this.scrollY == false){
-	backgroundTransition(false);
-}
- 	this.oldScroll = this.scrollY;
-}
-function backgroundTransition(x) {
-	if (x==true) {
-		document.body.classList.add("transition");
-		document.getElementsByClassName("acceuil__main-1__image")[0].classList.add("transition");
-		document.getElementsByClassName("acceuil__main-2__image")[0].classList.add("transition");
-	}
-	else if (x==false) {
-		document.body.classList.remove("transition");
-		document.getElementsByClassName("acceuil__main-1__image")[0].classList.remove("transition");
-		document.getElementsByClassName("acceuil__main-2__image")[0].classList.remove("transition");
-	}
-}
-}
-function defineHamburger(x) {
-	hamburger__clicked = true;
-	document.getElementById("hamburger__toggle").addEventListener("click", function() {
-		if (hamburger__clicked==true) {
-			document.getElementById("navbar").classList.add("transition");
-			document.getElementById(x+"__main").classList.add("transition");
-			hamburger__clicked = false;
-		}
-		else if (hamburger__clicked == false) {
-			document.getElementById("navbar").classList.remove("transition");
-			document.getElementById(x+"__main").classList.remove("transition");
-			hamburger__clicked = true;
-		}
-	});
-}
-function defineAbout() {
-	defineHamburger("about");
+window.addEventListener("scroll", (event) => {
+    var scroll = this.scrollY;
+    console.log(scroll)
+    document.documentElement.style.setProperty('--title-position', scroll + "px");
+    
+    console.log("position" , document.documentElement.style.getPropertyValue('--cursor-position'))
+    console.log("position ant" , document.documentElement.style.getPropertyValue('--anteater-position'))
+
+    if (scroll>window.screen.height) {
+        //console.log("limite")
+    }
+    defineSkillChart(scroll);
+    defineAnteater(scroll)
+});
+window.onload = () => {
+    document.getElementById("header").classList.add("onload");
 }
 
-function defineContact() {
-	defineHamburger("contact");
-}
-	
-defineScript()
 
-function defineScript() {
-	if (document.title == "Accueil") {
-		defineAcceuil();
-	}
-	else if (document.title == "About") {
-		defineAbout();
-	
-	}
-	else if (document.title == "Contact") {
-		defineContact();
-	}
+function defineAnteater(position) {
+    var anteaterPosition = (position/window.innerHeight).toFixed(2);
+    console.log("ant pos" , anteaterPosition)
+    document.documentElement.style.setProperty('--anteater-position', anteaterPosition);
+    
+
+}
+function defineSkillChart(position) {
+    var cursorPosition = (position/(window.innerHeight*2.4)).toFixed(2);
+    document.documentElement.style.setProperty('--cursor-position', cursorPosition);
+    if (cursorPosition>1) {
+        document.documentElement.style.setProperty('--cursor-position', 1);
+    }
 }
